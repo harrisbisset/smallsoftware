283e230b8caecba2492c3b9846140efd7d00c925
harrisbisset <RedstoneRebel399@gmail.com>
2026-02-08 23:34:24 +0000

    source link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,15 +7,20 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="flex min-h-lvh flex-col">
+<div class="flex min-h-lvh flex-col text-charcoal">
 	<div class="flex grow">
 		<div class="h-full w-full">
 			{@render children()}
 		</div>
 	</div>
 
-	<footer class="flex shrink bg-teal p-2">
+	<footer class="flex shrink bg-teal p-3">
 		<div class="text-nowrap">a U.K. tech company</div>
-		<div class="w-full flex justify-end">source: </div>
+		<div class="flex w-full justify-end gap-1.5">
+			<div>source:</div>
+			<a class="underline decoration-charcoal" href="https://github.com/harrisbisset/smallsoftware">
+				github.com
+			</a>
+		</div>
 	</footer>
 </div>
